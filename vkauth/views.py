e8a8ce2d92cedfb60e6105e6e51a91abf5db8fb8
nightcore194 <58079281+nightcore194@users.noreply.github.com>
2021-04-02 17:02:52 +0500

    v0.028

--- vkauth/views.py
+++ vkauth/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from social_django.models import UserSocialAuth
 from .models import vk_user_id, vk_user_token, vk_user_stat, vk_user_login
-import json
+import json, urllib3, requests
 from django.core import serializers
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
@@ -31,21 +31,18 @@ def get_stat(request):
     user_data = UserSocialAuth.objects.get(user=user)
     user_data_parser = user_data.extra_data
     #auth login
-    vk_stat = vk_api.VkApi(user_data_parser["access_token"])
-    vk_stat.auth(token_only=True, login=False)
-    vk = vk_stat.get_api()
     #get group
     vk_stat_get = vk_user_stat()
     vk_user_ids = vk_user_id()
     vk_user_ids.id_user = user_data_parser["id"]
     vk_user_ids.save()
-    vkgroup = vk.groups.get(id=user_data_parser["id"], filter='admin')
-    vkgroup = json.dumps(vkgroup["items"])
-    vkgroup = json.loads(vkgroup)
+    vkgroup = requests.get("https://api.vk.com/method/groups.get?id="+str(user_data_parser["id"])+"&filter=admin&access_token="+str(user_data_parser["access_token"])+"&v=5.130")
+    vkgroup = json.loads(vkgroup.json())
     #get stats
-    vks = vk.stats.get(group_id=vkgroup[0], interval='week', intervals_count=1, stats_group='reach', extended=0)
-    vks = json.dumps(vks[0]["reach"])
-    vks = json.loads(vks)
+    vks = requests.get("https://api.vk.com/method/stats.get?group_id="+str(vkgroup[0])+"&interval=week&intervals_counts=1&stats_group=reach&extended=0&access_token="+str(user_data_parser["access_token"])+"&v=5.130")
+    #vks = vk.stats.get(group_id=vkgroup[0], interval='week', intervals_count=1, stats_group='reach', extended=0)
+    #vks = json.dumps(vks.json("reach"))
+    vks = json.loads(vks.json())
     vk_stat_get.id_user = vk_user_id.objects.get(id_user=user_data_parser["id"])
     vk_stat_get.reach_by_week = vks["reach"]
     vk_stat_get.save()
