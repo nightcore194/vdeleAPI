0f60cea292a499354eed9d84cd4de61ff0d25311
nightcore194 <58079281+nightcore194@users.noreply.github.com>
2021-04-04 13:14:19 +0500

    v0.03
    -auth vk is working!

--- vkauth/models.py
+++ vkauth/models.py
@@ -14,16 +14,16 @@ class vk_user_id(models.Model): #id юзеров
         return str(self.id_user)
 
 class vk_user_token(models.Model): #access_token
-    token = models.CharField(max_length=1000, default="none")
-    login = models.OneToOneField(vk_user_login, to_field='login', on_delete=models.CASCADE),
+    token = models.CharField(max_length=1000, default=0)
+    login_for = models.OneToOneField(vk_user_login, to_field='login', on_delete=models.CASCADE, default=0)
 
 
     def __str__(self):
         return str(self.token)
 
 class vk_user_stat(models.Model):#stat юзера
-    reach_by_week = models.CharField(max_length=1000),
-    id_user = models.OneToOneField(vk_user_id, to_field='id_user', on_delete=models.CASCADE)
+    reach_by_week = models.CharField(max_length=1000, default=0)
+    id_user_for = models.OneToOneField(vk_user_id, to_field='id_user', on_delete=models.CASCADE, default=0)
 
     def __str__(self):
         return str(self.reach_by_week)
--- vkauth/views.py
+++ vkauth/views.py
@@ -5,7 +5,6 @@ import json, urllib3, requests
 from django.core import serializers
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
-import vk_api
 #main page view
 def index(request):
     if request.user.is_authenticated:
@@ -35,19 +34,23 @@ def get_stat(request):
     vk_user_ids = vk_user_id()
     vk_user_ls = vk_user_login()
     token = vk_user_token()
-    token.login = user_data_parser["id"]
-    token.token = user_data_parser["access_token"]
     vk_user_ls.login = user_data_parser["id"]
-    vkgroup = requests.get("https://api.vk.com/method/groups.get?id="+str(user_data_parser["id"])+"&filter=admin&access_token="+str(user_data_parser["access_token"])+"&v=5.130")
-    vkgroup = json.loads(vkgroup.json())
-    vk_user_ids.id_user = vkgroup[0]
-    #get stats
-    vks = requests.get("https://api.vk.com/method/stats.get?group_id="+str(vkgroup[0])+"&interval=week&intervals_counts=1&stats_group=reach&extended=0&access_token="+str(user_data_parser["access_token"])+"&v=5.130")
-    vks = json.loads(vks.json())
-    vk_stat_get.id_user = vk_user_id.objects.get(id_user=user_data_parser["id"])
-    vk_stat_get.reach_by_week = vks["reach"]
-    token.save()
-    vk_user_ids.save()
     vk_user_ls.save()
+    token.login_for = vk_user_login.objects.get(login=user_data_parser["id"])
+    token.token = user_data_parser["access_token"]
+    vkgroup = requests.get("https://api.vk.com/method/groups.get?id="+str(user_data_parser["id"])+"&filter=admin&access_token="+str(user_data_parser["access_token"])+"&v=5.130")
+    vkgroupjs = vkgroup.json()
+    vkgroupjs = vkgroupjs["response"]
+    vkgroupjs = vkgroupjs["items"]
+    vk_user_ids.id_user = vkgroupjs[0]
+    vk_user_ids.save()
+    #get stats
+    vks = requests.get("https://api.vk.com/method/stats.get?group_id="+str(vkgroupjs[0])+"&interval=week&intervals_count=1&stats_group=reach&extended=0&access_token="+str(user_data_parser["access_token"])+"&v=5.130")
+    vk = vks.json()
+    vkjs = vk["response"]
+    vkjs = vkjs[0]["reach"]
+    vk_stat_get.id_user_for = vk_user_id.objects.get(id_user=vkgroupjs[0])
+    vk_stat_get.reach_by_week = vkjs["reach"]
+    token.save()
     vk_stat_get.save()
     return render(request, 'stat.html')
